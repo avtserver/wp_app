36732ed52dc5fd1ab564949a0c1810fd0d894ddc
Shunya Academy <sacademy@frappe-prod-030721.asia-south1-c.c.basic-cabinet-312506.internal>
2021-07-04 11:47:54 +0530

    add

--- wp_app/www/wp-member/index.py
+++ wp_app/www/wp-member/index.py
@@ -2,7 +2,7 @@ import frappe
 
 def get_context(context):
 
-    opmember = frappe.db.sql("""SELECT name, name1, designation, profile_image, cover_image, short_bio_data, about_me, email, mobile_number FROM `tabOriflame Premium Member` ORDER BY creation DESC;""",
+    opmember = frappe.db.sql("""SELECT name, name1, designation, profile_image, short_intro, about_me, email, mobile_number FROM `tabOriflame Premium Member` ORDER BY creation DESC;""",
                                as_dict=True)
     context.opmember = opmember
 
